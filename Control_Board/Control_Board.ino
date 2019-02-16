46324b2b7845ebdbcdba94aaaf9027488c8a7df0
tenbot <47680501+tenbot@users.noreply.github.com>
2019-02-16 03:19:36 +0100

    Initial project layout

---
+++ Control_Board/Control_Board.ino
@@ -0,0 +1,13 @@
+/*
+ * Board manager URL: http://dan.drown.org/stm32duino/package_STM32duino_index.json
+ * Board type : Generic STM32F103C
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Dashboard/Dashboard.ino
@@ -0,0 +1,13 @@
+/*
+ * Board manager URL: https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json
+ * Board type : Generic nRF51
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
