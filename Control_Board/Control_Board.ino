d79eddca16adffecf3083134fe28f701bef46927
tenbot <47680501+tenbot@users.noreply.github.com>
2019-02-27 01:47:33 +0100

    First pass at implementing Tenbot messages

--- Control_Board/Control_Board.ino
+++ Control_Board/Control_Board.ino
@@ -1,7 +1,31 @@
 /*
+ *  _______         ____        _   
+ * |__   __|       |  _ \      | |  
+ *    | | ___ _ __ | |_) | ___ | |_ 
+ *    | |/ _ \ '_ \|  _ < / _ \| __|
+ *    | |  __/ | | | |_) | (_) | |_ 
+ *    |_|\___|_| |_|____/ \___/ \__|
+ *                                  
+ *    Control Board Firmware                          
  * Board manager URL: http://dan.drown.org/stm32duino/package_STM32duino_index.json
  * Board type : Generic STM32F103C
  */
+bool isScooterTurnedOn = false;
+bool isScooterLocked = false;
+/* 
+ * TenbotMessages protocol implementation
+ * 
+ * TenbotMessages follow this scheme : 1 byte command + 1 to 16 int payload (can be omitted if the command doesn't require it)
+ * For more details on the protocol, please check the Wiki page
+ * Dashboard <-> Control Board messsages :
+ */
+#define SET_SCOOTER_STATE 0x01
+#define SET_SCOOTER_LOCK 0x02
+#define BRAKE_THROTTLE_UPDATE 0x03
+#define SPEED_UPDATE 0x04
+#define SETTINGS_UPDATE 0x05
+#define SET_HEADLIGHT_STATE 0x06
+#define BATTERY_STATE 0x07
 void setup() {
   // put your setup code here, to run once:
 
@@ -10,4 +34,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+void dashboardCommandHandler(byte command, int payload[16]) {
+  int brakeValue;
+  int throttleValue;
+  switch (command) {
+    case SET_SCOOTER_LOCK:
+      isScooterLocked = payload[0];
+      // TODO: set the real lock...
+      break;
+    case BRAKE_THROTTLE_UPDATE:
+      brakeValue = payload[0];
+      throttleValue = payload[1];
+      // TODO: Power the motor according to these values, and make the rearlight blink when braking 
+      break;
+    case SETTINGS_UPDATE:
+      int setting = payload[0];
+      //updateSetting(setting, &payload[1]);
+      break;
+  }
 }
--- Dashboard/Dashboard.ino
+++ Dashboard/Dashboard.ino
@@ -1,13 +1,78 @@
+#include <TM1650.h>
+#include <Wire.h>
 /*
+ *  _______         ____        _   
+ * |__   __|       |  _ \      | |  
+ *    | | ___ _ __ | |_) | ___ | |_ 
+ *    | |/ _ \ '_ \|  _ < / _ \| __|
+ *    | |  __/ | | | |_) | (_) | |_ 
+ *    |_|\___|_| |_|____/ \___/ \__|
+ *                                  
+ *    Dashboard Firmware 
  * Board manager URL: https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json
  * Board type : Generic nRF51
  */
+TM1650 d;
+#define SDA 4 // Temporary pins mapping
+#define SCL 6
+#define buzzer 7
+#define throttle 8
+#define brake 9
+#define uart 10
+#define headlight 11
+bool isScooterTurnedOn = false;
+bool isScooterLocked = false;
+bool isHeadlightTurnedOn = false;
+/* 
+ * TenbotMessages protocol implementation
+ * 
+ * TenbotMessages follow this scheme : 1 byte command + 1 to 16 int payload (can be omitted if the command doesn't require it)
+ * For more details on the protocol, please check the Wiki page
+ * Dashboard <-> Control Board messsages :
+ */
+#define SET_SCOOTER_STATE 0x01
+#define SET_SCOOTER_LOCK 0x02
+#define BRAKE_THROTTLE_UPDATE 0x03
+#define SPEED_UPDATE 0x04
+#define SETTINGS_UPDATE 0x05
+#define SET_HEADLIGHT_STATE 0x06
+#define BATTERY_STATE 0x07
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.setPins(SDA, SCL);
+  Wire.begin();
+  pinMode(throttle, INPUT);
+  pinMode(brake, INPUT);
+  pinMode(headlight, OUTPUT);
+  d.init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+}
+void controlBoardCommandHandler(byte command, int payload[16]) {
+  int currentSpeed;
+  int batteryState;
+  switch (command) {
+    case SET_SCOOTER_STATE:
+      isScooterTurnedOn = payload[0];
+      // TODO: turn on or off the dashboard
+      analogWrite(buzzer, 127);
+      delay(200);
+      digitalWrite(buzzer, LOW);
+      break;
+    case SPEED_UPDATE:
+      currentSpeed = payload[0];
+      // TODO: Display the speed on the screen using I²C
+      break;
+    case SET_HEADLIGHT_STATE:
+      isHeadlightTurnedOn = payload[0];
+      digitalWrite(headlight, isHeadlightTurnedOn);
+      break;
+    case BATTERY_STATE:
+      batteryState = payload[0];
+      // TODO: Display the battery level on the screen using I²C
+      break;
+  }
 }
